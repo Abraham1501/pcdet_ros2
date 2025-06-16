eb481124e69c3754b9b787edbb8b776f3e737cb9
Abraham1501 <abraham.mata15012006@gmail.com>
2025-06-16 17:25:04 -0600

    first commit - Updates for cuda 12, it works

--- setup.sh
+++ setup.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
 git clone https://github.com/open-mmlab/OpenPCDet
+# torch 2.7.1; torchuaudio 2.7.1; torchvision 0.22.1;
 python3 -m pip install torch torchvision torchaudio
-python3 -m pip install tensorflow
-python3 -m pip install spconv-cu117
+python3 -m pip install tensorflow==2.19.0
+python3 -m pip install spconv-cu120==2.3.6
+cp ./extra/setup.py ./OpenPCDet/
 cd OpenPCDet
 python3 -m pip install -r requirements.txt
 python3 setup.py develop
-python3 -m pip install kornia open3d
-python3 -m pip install pyquaternion
+python3 -m pip install kornia==0.6.12 open3d==0.19.0
+python3 -m pip install pyquaternion==0.9.9
 cd ..
 cd src/
 git clone https://github.com/Box-Robotics/ros2_numpy -b humble
 python3 -m pip install catkin_pkg
 sudo apt install ros-humble-ament-cmake-nose -y
-python3 -m pip install nose
-python3 -m pip install transform3d
+python3 -m pip install nose==1.3.7
+python3 -m pip install av2==0.3.4
+python3 -m pip install transform3d==0.0.4
+python3 -m pip install numpy==1.26.4
+sudo apt-get update && sudo apt-get install -y \
+    ros-humble-sensor-msgs \
+    ros-humble-nav-msgs \
+    ros-humble-geometry-msgs \
+    ros-humble-vision-msgs \
+    ros-humble-visualization-msgs \
+    ros-humble-pcl-conversions \
+    ros-humble-pcl-ros \
+    ros-humble-navigation2 \
+    ros-humble-nav2-bringup \
+    ros-humble-tf-transformations \
+    libpcl-dev
 cd ..
-rosdep install -i --from-path src --rosdistro humble -y
 colcon build --symlink-install
