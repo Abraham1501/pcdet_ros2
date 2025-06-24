561942cbe66b4a03cab1898f1207365635fa225f
Abraham1501 <abraham.mata15012006@gmail.com>
2025-06-23 20:50:21 -0600

    x86_64 dockerfile works, some general erros fixed

--- create_container.sh
+++ create_container.sh
@@ -4,15 +4,18 @@ cd src/
 git clone https://github.com/Box-Robotics/ros2_numpy -b humble
 cd ..
 
-IMAGE_NAME=sdv_pcdet-ros2
-CONTAINER_NAME=sdv_pcdet-ros2
+IMAGE_NAME=sdv_pcdet-ros2_cuda12
+CONTAINER_NAME=sdv_pcdet-ros2_cuda12
 
 DOCKER_COMMAND="docker run"
 
 xhost +
 
 $DOCKER_COMMAND -it -d \
+    --gpus all \
     --network=host \
+    --ipc=host \
+    --pid=host \
     --privileged \
     -v /dev:/dev \
     -v "$PWD/src:/ws/src" \
@@ -20,4 +23,4 @@ $DOCKER_COMMAND -it -d \
     $IMAGE_NAME \
     bash
 
-docker cp ./extra/setup.py sdv_pcdet-ros2:ws/OpenPCDet
+docker cp ./extra/setup.py sdv_pcdet-ros2_cuda12:ws/OpenPCDet
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/open-mmlab/OpenPCDet
-# torch 2.7.1; torchuaudio 2.7.1; torchvision 0.22.1;
-python3 -m pip install torch torchvision torchaudio
+# For CUDA 12.1
+torch==2.2.2+cu121 torchvision==0.17.2+cu121 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu121 \
 python3 -m pip install tensorflow==2.19.0
 python3 -m pip install spconv-cu120==2.3.6
 cp ./extra/setup.py ./OpenPCDet/
@@ -19,6 +19,7 @@ sudo apt install ros-humble-ament-cmake-nose -y
 python3 -m pip install nose==1.3.7
 python3 -m pip install av2==0.3.4
 python3 -m pip install transform3d==0.0.4
+python3 -m pip install transforms3d==0.4.2
 python3 -m pip install numpy==1.26.4
 sudo apt-get update && sudo apt-get install -y \
     ros-humble-sensor-msgs \
